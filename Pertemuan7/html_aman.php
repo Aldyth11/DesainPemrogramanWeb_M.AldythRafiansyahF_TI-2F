afb27da1e6157a9b28dc3e72ae505f0172566b03
Aldyth11 <aldyth.r.f@gmail.com>
2025-10-11 10:24:09 +0700

    Praktikum 4 : HTML Injection

---
+++ Pertemuan7/html_aman.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>HTML Injection</title>
+</head>
+<body>
+<h2>Form Input</h2>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+    <label for="input">Input Teks:</label>
+    <input type="text" name="input" id="input" required><br><br>
+    <label for="email">Input Email:</label>
+    <input type="text" name="email" id="email" required><br><br>
+    <input type="submit" value="Submit">
+</form>
+<br><br>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    if (isset($_POST['input'])) {
+        $input = $_POST['input'];
+        $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
+        echo '<div>Input aman: ' . $input . '</div>';
+    } else {
+    
+    }
+    
+    if (isset($_POST['email'])) { 
+        $email = $_POST['email'];
+        
+        $clean_email = filter_var($email, FILTER_SANITIZE_EMAIL); 
+        
+        if (filter_var($clean_email, FILTER_VALIDATE_EMAIL)) {
+            echo "Email valid dan aman: " . htmlspecialchars($clean_email) . "<br>";
+        } else {
+            echo "Email tidak valid. masukkan format email yang benar.<br>";
+        }
+    } else {
+        echo "Input email tidak ditemukan.<br>";
+    }
+
+}
+?>
+?>
+</body>
+</html>
